43167c65010d748a34bd25347e7d89884e2be710
Danica-f <djn3278@WMICH.EDU>
2026-02-17 15:01:33 -0500

    learning_bash.sh
    Assignment #2

---
+++ learning_bash.sh
@@ -0,0 +1,8 @@
+greeting=Hello
+your_name=Danica
+echo "$greeting $your_name";echo "This scriot is helping you learn bash"
+mkdir bioinformatics
+cd bioinformatics 
+pkdir bioinformatics/bash_scripts
+mv ~/Desktop/bash_template.txt /Users/danicalfox/bioinformatics/bioinformatics/bash_scripts
+mv bash_template.text bash_template.sh
