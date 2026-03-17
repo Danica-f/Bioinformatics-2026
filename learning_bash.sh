7ea318d282e22a4c2c03baad99a4641fc210ba4c
Danica-f <djn3278@WMICH.EDU>
2026-03-17 18:46:18 -0400

    Update learning_bash.sh

--- learning_bash.sh
+++ learning_bash.sh
@@ -4,6 +4,6 @@ your_name=Danica
 echo "$greeting $your_name";echo "This script is helping you learn bash"
 mkdir bioinformatics
 cd bioinformatics 
-pkdir bioinformatics/bash_scripts
+mkdir bioinformatics/'bash_scripts'
 mv ~/Desktop/bash_template.txt /Users/danicalfox/bioinformatics/bash_scripts
 mv bash_template.txt bash_template.sh
